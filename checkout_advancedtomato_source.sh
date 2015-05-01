1ee5c7bc620acd34e1fb2e1a657946877d507185
Tony Arnold <tony@thecocoabots.com>
2015-05-02 00:47:27 +1000

    Fix broken script

--- checkout_advancedtomato_source.sh
+++ checkout_advancedtomato_source.sh
@@ -8,7 +8,7 @@ else
     git clone https://bitbucket.org/jackysi/advancedtomato-arm.git advancedtomato-arm
 fi
 
-if [! -d /home/tomato/advancedtomato-arm ]; then
+if [ ! -d /home/tomato/advancedtomato-arm ]; then
     ln -s /home/tomato/advancedtomato-arm/release/src-rt-6.x.4708/toolchains/hndtools-arm-linux-2.6.36-uclibc-4.5.3/ /opt/hndtools-arm-linux-2.6.36-uclibc-4.5.3
 fi
 
